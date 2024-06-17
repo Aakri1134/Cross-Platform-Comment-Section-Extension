01291dca16315e77fdb1579d57d3b5651d4c3972
x20surya <suryanshuc659@gmail.com>
2024-06-17 17:51:42 +0530

    Update credentials.js

--- Cross-Platform-Comment-Section/src/constants/credentials.js
+++ Cross-Platform-Comment-Section/src/constants/credentials.js
@@ -1,10 +1,3 @@
 export const firebaseConfig = {
   //Your Credentials (Be sure to not push it)
-  apiKey: "AIzaSyBPmcya3ReSu3Xm3Uvtzx-ZCOqQ-X_Q6A4",
-  authDomain: "test-app-81ffd.firebaseapp.com",
-  projectId: "test-app-81ffd",
-  storageBucket: "test-app-81ffd.appspot.com",
-  messagingSenderId: "813803682779",
-  appId: "1:813803682779:web:5aefdb5a5bfb05b2d505fe",
-  measurementId: "G-X8RY8SMNVW",
 };
