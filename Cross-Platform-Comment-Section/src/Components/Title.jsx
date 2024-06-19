9bd390602214dab442c17b230714ef5b693bbb82
x20surya <suryanshuc659@gmail.com>
2024-06-19 15:17:25 +0530

    Add Design on Title and PostComment

--- Cross-Platform-Comment-Section/src/Components/Title.jsx
+++ Cross-Platform-Comment-Section/src/Components/Title.jsx
@@ -8,7 +8,7 @@ const Title = (props) => {
         setTitle(room)
     },[room])
   return (
-    <div className="flex-none h-auto text-left text-wrap text-3xl text-gray-900 font-bold font-sans leading-9 shadow-lg pt-3 pr-4 pl-4 pb-3">
+    <div className="flex-none h-auto text-left text-wrap text-xl text-primary-100 font-bold font-sans leading-9 shadow-lg pt-3 pr-4 pl-4 pb-3 bg-gray-50 dark:bg-gray-900">
       <h1>{title}</h1>
     </div>
   );
