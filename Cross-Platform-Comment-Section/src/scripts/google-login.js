b3052be486437f136237ef61eb8eed8472973779
x20surya <suryanshuc659@gmail.com>
2024-06-20 21:11:03 +0530

    Update google-login.js
    Fixed bug, 2 times google sign in pop up

--- Cross-Platform-Comment-Section/src/scripts/google-login.js
+++ Cross-Platform-Comment-Section/src/scripts/google-login.js
@@ -1,72 +1,56 @@
-import qs from "qs"
-
+import qs from "qs";
 
 // ─────────────────────────────────────────────────────────────────────────────
 
 export const getAuthToken = async () => {
   try {
     //
-    const { access_token } = await launchWebAuthFlow_and_getAccessToken()
-    const { id_token } = await launchWebAuthFlow_and_getTokenId()
-    console.log(access_token);
+    const { access_token } = await launchWebAuthFlow_and_getAccessToken();
     return access_token;
     //
   } catch (error) {
     console.error(`error-googleLogin`, error);
     return null;
   }
-}
+};
 
 // ─────────────────────────────────────────────────────────────────────────────
 
 const launchWebAuthFlow_and_getAccessToken = async () => {
-  const manifest = chrome.runtime.getManifest()
-  const { url } = generateBaseUrl_and_redirectUri()
+  const manifest = chrome.runtime.getManifest();
+  const { url } = generateBaseUrl_and_redirectUri();
 
-  url.searchParams.set("response_type", "token")
-  url.searchParams.set("scope", manifest.oauth2.scopes.join(" "))
+  url.searchParams.set("response_type", "token");
+  url.searchParams.set("scope", manifest.oauth2.scopes.join(" "));
 
   const { access_token } = await launchWebAuthFlow_and_returnCallBackParams({
-    url
-  })
+    url,
+  });
 
-  return { access_token }
-}
-
-const launchWebAuthFlow_and_getTokenId = async () => {
-  const { url } = generateBaseUrl_and_redirectUri()
-
-  url.searchParams.set("response_type", "id_token")
-  url.searchParams.set("access_type", "offline")
-  url.searchParams.set("scope", "openid email profile") // only & fixed scopes for get id_token
-
-  const { id_token } = await launchWebAuthFlow_and_returnCallBackParams({ url })
-
-  return { id_token }
-}
-
+  return { access_token };
+};
 
 // ─────────────────────────────────────────────────────────────────────────────
 
 const generateBaseUrl_and_redirectUri = () => {
-  const manifest = chrome.runtime.getManifest()
+  const manifest = chrome.runtime.getManifest();
 
   // https://developers.google.com/identity/protocols/oauth2/javascript-implicit-flow#redirecting
-  const url = new URL("https://accounts.google.com/o/oauth2/auth")
+  const url = new URL("https://accounts.google.com/o/oauth2/auth");
 
   // NOTE:
   // save EXACT redirectUri on google cloud console > credentials > OAuth 2.0 Client IDs > Web client
   // save EXACT redirectUri on supabase > Authentication > URL Configuration > Redirect URLs
-  const redirectUri = chrome.identity.getRedirectURL("firebase-auth")
+  const redirectUri = chrome.identity.getRedirectURL("firebase-auth");
   console.log(redirectUri);
   // console.log(`redirectUri`, redirectUri) // -> https://dekkcnhhmkcahbhnaphlibnodmigphnc.chromiumapp.org/supabase-auth
 
-  url.searchParams.set("client_id", manifest.oauth2.client_id)
-  url.searchParams.set("redirect_uri", redirectUri)
+  url.searchParams.set("client_id", manifest.oauth2.client_id);
+  url.searchParams.set("redirect_uri", redirectUri);
   url.searchParams.set("prompt", "select_account");
 
-  return { url }
-}
+  return { url };
+};
 
 // ─────────────────────────────────────────────────────────────────────────────
 
@@ -75,11 +59,11 @@ const launchWebAuthFlow_and_returnCallBackParams = async ({ url }) => {
     chrome.identity.launchWebAuthFlow(
       { url: url.href, interactive: true },
       (callbackUrl) => resolve(callbackUrl)
-    )
-  })
-  if (!authorizeResult) throw { error: "No authorizeResult" }
+    );
+  });
+  if (!authorizeResult) throw { error: "No authorizeResult" };
 
-  return qs.parse(authorizeResult?.split("#")[1])
-}
+  return qs.parse(authorizeResult?.split("#")[1]);
+};
 
 // ─────────────────────────────────────────────────────────────────────────────
