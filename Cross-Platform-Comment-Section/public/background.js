d055931d5a3104affa67000b517e973c8554097d
Aakrisht Srivastava <aakrisri55@gmail.com>
2024-06-20 15:52:37 +0530

    Add Solution to Netflix Problem

--- Cross-Platform-Comment-Section/public/background.js
+++ Cross-Platform-Comment-Section/public/background.js
@@ -8,6 +8,7 @@ chrome.tabs.onActivated.addListener((a) => {
     chrome.tabs.query({ active: true, currentWindow: true },async (tabs) => {
 
         let tabId = tabs[0].id
+        console.log("activated")
 
         if (!tabs[0].url) {
             await chrome.sidePanel.setOptions({
@@ -26,16 +27,33 @@ chrome.tabs.onActivated.addListener((a) => {
                 enabled: false
             });
         }else{
-            await chrome.sidePanel.setOptions({
-                tabId,
-                path: 'index.html',
-                enabled: true
-            });
+            if(url.origin === "https://www.youtube.com" || url.origin === "https://www.netflix.com"){
+                if(url.pathname.includes("/watch")){
+                    await chrome.sidePanel.setOptions({
+                        tabId,
+                        path: 'index.html',
+                        enabled: true
+                    });
+                }else{
+                    await chrome.sidePanel.setOptions({
+                        tabId,
+                        enabled: false
+                    });
+                }
+            }else{
+                await chrome.sidePanel.setOptions({
+                    tabId,
+                    path: 'index.html',
+                    enabled: true
+                });
+            }
+            
         }
     })
 })
 chrome.tabs.onUpdated.addListener((a,b,c) => {
     chrome.tabs.query({ active: true, currentWindow: true },async (tabs) => {
+        console.log("updated")
         let tabId = tabs[0].id
         if (!tabs[0].url) {
             await chrome.sidePanel.setOptions({
@@ -54,11 +72,27 @@ chrome.tabs.onUpdated.addListener((a,b,c) => {
                 enabled: false
             });
         }else{
-            await chrome.sidePanel.setOptions({
-                tabId,
-                path: 'index.html',
-                enabled: true
-            });
+            if(url.origin === "https://www.youtube.com" || url.origin === "https://www.netflix.com"){
+                if(url.pathname.includes("/watch")){
+                    await chrome.sidePanel.setOptions({
+                        tabId,
+                        path: 'index.html',
+                        enabled: true
+                    });
+                }else{
+                    await chrome.sidePanel.setOptions({
+                        tabId,
+                        enabled: false
+                    });
+                }
+            }else{
+                await chrome.sidePanel.setOptions({
+                    tabId,
+                    path: 'index.html',
+                    enabled: true
+                });
+            }
+            
         }
     })
 })
